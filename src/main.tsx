11fe14e4ecba4c156f3b842cd8a6a9f90c8e619b
mia <mia@mia-MacBookPro.local>
2024-10-27 16:14:58 +0900

    set styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Router.tsx";
+import { GlobalStyle } from "./GlobalStyle.tsx";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>
 );
